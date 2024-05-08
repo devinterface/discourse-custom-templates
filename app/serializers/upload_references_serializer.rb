f695396af39df96b514cfa764859fd0481938dc6
Loris Oppia <loris.oppia@devinterface.com>
2024-05-08 10:55:57 +0200

    Remove controllers and serializers

--- app/serializers/upload_references_serializer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class UploadReferencesSerializer < ApplicationSerializer
-  attributes :target_type
-
-  has_one :target, embed: :object, serializer: TargetSerializer
-end
